00a2e09b943ae1d75a5341529ef6bde55d81705a
zy414266852 <414266852@qq.com>
2017-05-02 14:13:01 +0800

    Homepage

--- YuJia/HomePageViewController.m
+++ YuJia/HomePageViewController.m
@@ -7,21 +7,71 @@
 //
 
 #import "HomePageViewController.h"
+#import "UIColor+Extension.h"
 
 @interface HomePageViewController ()
 
 @end
 
-@implementation HomePageViewController
+@implementation HomePageViewController{
+    UIImageView *navBarHairlineImageView;
+    UIImageView *tabBarHairlineImageView;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 //    self.title = @"家";
     self.view.backgroundColor = [UIColor whiteColor];
     self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    
+    UILabel *titleLabel = [[UILabel alloc]initWithFrame:CGRectMake(10, 16, 60, 12)];
+    titleLabel.text = @"切换实景";
+    titleLabel.textColor = [UIColor colorWithHexString:@"333333"];
+    titleLabel.font = [UIFont systemFontOfSize:12];
+    
+    
+    UIButton *leftNavBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    leftNavBtn.frame = CGRectMake(10, 16, 60, 12);
+    [leftNavBtn setTitle:@"切换实景" forState:UIControlStateNormal];
+    [leftNavBtn setTitleColor:[UIColor colorWithHexString:@"333333"] forState:UIControlStateNormal];
+    leftNavBtn.titleLabel.font = [UIFont systemFontOfSize:12];
+    [leftNavBtn addTarget:self action:@selector(action:) forControlEvents:UIControlEventTouchUpInside];
+    [self.navigationController.navigationBar addSubview:leftNavBtn];
+    
+    
+    self.navigationController.navigationBar.barTintColor = [UIColor whiteColor];
+    
+    // 改变navBar 下面的线
+    UINavigationBar *navigationBar = self.navigationController.navigationBar;
+//    UIImageView *imageViewHead = [[UIImageView alloc]initWithFrame:CGRectMake(0, navigationBar.frame.size.height +20, kScreenW, 3)];
+//    imageViewHead.image = [UIImage imageNamed:@"headshadow"];
+//    imageViewHead.contentMode = UIViewContentModeScaleToFill;
+//    [self.view addSubview:imageViewHead];
+    navBarHairlineImageView = [self findHairlineImageViewUnder:navigationBar];
+    UILabel *coverView = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, kScreenW, 1)];
+    coverView.backgroundColor = [UIColor colorWithHexString:@"e9e9e9"];
+    [navBarHairlineImageView removeFromSuperview];
+    [navBarHairlineImageView addSubview:coverView];
+    
+    
     // Do any additional setup after loading the view.
 }
-
+/**
+ * PS:navigation  下面的线
+ */
+- (UIImageView *)findHairlineImageViewUnder:(UIView *)view {
+    if ([view isKindOfClass:UIImageView.class] && view.bounds.size.height <= 1.0) {
+        return (UIImageView *)view;
+    }
+    for (UIView *subview in view.subviews) {
+        UIImageView *imageView = [self findHairlineImageViewUnder:subview];
+        if (imageView) {
+            return imageView;
+        }
+    }
+    return nil;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
